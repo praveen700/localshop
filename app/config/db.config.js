40a1192b830ad5b4cfa7364e6e1f273a186c5e33
praveen.mr <praveen.mr@datafoundry.ai>
2023-04-04 23:47:15 +0530

    added-env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,11 @@
 const db = require("./config");
+const mysql  = require("mysql2/promise");
 
-module.exports = {
+const pool = mysql.createPool({
   HOST: db.DB_HOST,
   USER: db.DB_USER,
   PASSWORD: db.DB_PASSWORD,
   DB: db.DB_NAME
-};
+})
+
+module.exports = {pool}
