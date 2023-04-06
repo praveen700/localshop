e82696b4281eb58f7264bcd2231738c50237431e
praveen.mr <praveen.mr@datafoundry.ai>
2023-04-06 12:00:43 +0530

    adddes-dotenv

--- app/config/config.js
+++ app/config/config.js
@@ -1,9 +1,9 @@
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_USER = process.env.DB_USER || 'root'
-const DB_PASSWORD = process.env.DB_PASSWORD || 'Praveen@6470'
-const DB_NAME = process.env.DB_NAME || 'ecommerace'
-const DB_PORT = process.env.DB_PORT || 3306
-
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+const DB_PORT = process.env.DB_PORT
+console.log(process.env, "process.env.DB_HOST")
 module.exports ={
     DB_HOST,
     DB_PASSWORD,
